68d761b74594b7d03e515e92c2ecd2ecd1f4577c
Takumi Crary <tcrary@gmail.com>
2021-05-19 09:01:17 +0100

    Start working some of the ideas from TaintedExample1 into the ND idea

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,49 @@
-{-# LANGUAGE GADTs, StandaloneDeriving #-}
-{-# LANGUAGE ConstraintKinds, DataKinds, TypeFamilies, TypeOperators, TypeApplications #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Main where
 
-type HasShowEq a = (Show a, Eq a)
+import GHC.Generics
+import Data.Proxy
+import Data.Typeable
+import Data.Type.Equality
+import Data.Maybe
 
-data TProofExpr a where
-    TAxiom        :: (HasShowEq a)              => a -> TProofExpr a
-    TAnd          :: (HasShowEq a, HasShowEq b) => ProofExpr a -> ProofExpr b -> TProofExpr (a,b)
-    TExtractLeft  :: (HasShowEq a, HasShowEq b) => ProofExpr (a,b) -> TProofExpr a
-    TExtractRight :: (HasShowEq a, HasShowEq b) => ProofExpr (a,b) -> TProofExpr b
+data PTaintedness -- Burden of Proof
+    = Burdened
+    | Unburdened
+    deriving (Eq, Show, Typeable)
+
+type family CombinePTaintedness (a :: PTaintedness) (b :: PTaintedness) :: PTaintedness where
+    'Burdened `CombinePTaintedness` _ = 'Burdened
+    _ `CombinePTaintedness` 'Burdened = 'Burdened
+    _ `CombinePTaintedness` _ = 'Unburdened
+
+type family IsProofMarked' (t :: PTaintedness) a :: Bool where
+    IsProofMarked' 'Burdened _ = 'True
+    IsProofMarked' 'Unburdened _ = 'False
+
+type IsProofMarked t a = (IsProofMarked' t a ~ 'True)
+type TEQShow a = (Typeable a, Eq a, Show a)
 
 data ProofExpr a where
-    Axiom        :: (HasShowEq a)              => a -> ProofExpr a
-    And          :: (HasShowEq a, HasShowEq b) => ProofExpr a -> ProofExpr b -> ProofExpr (a,b)
-    ExtractLeft  :: (HasShowEq a, HasShowEq b) => ProofExpr (a,b) -> ProofExpr a
-    ExtractRight :: (HasShowEq a, HasShowEq b) => ProofExpr (a,b) -> ProofExpr b
-    DischargeImply :: (HasShowEq a, HasShowEq b) => TProofExpr a -> b -> ProofExpr b
+    Axiom        :: (TEQShow a)              => a -> ProofExpr a
 
-deriving instance (Show a) => Show (ProofExpr a)
-deriving instance (Show a) => Show (TProofExpr a)
+    AndIntro          :: (TEQShow a, TEQShow b) => ProofExpr a -> ProofExpr b -> ProofExpr (a,b)
+    ExtractLeftExtro  :: (TEQShow a, TEQShow b) => ProofExpr (a,b) -> ProofExpr a
+    ExtractRightExtro :: (TEQShow a, TEQShow b) => ProofExpr (a,b) -> ProofExpr b
+    --ImplyIntro     :: Given an Expr B that recursively contains Tainted A annotated Z,
+    --               :: (ContainsBurdenZ b a) => b -> (a->b)
+    --               Somehow extract the a from b?
+    DischargeImplyExtro :: (TEQShow a, TEQShow b) => ProofExpr (a -> b) -> a -> ProofExpr b
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
